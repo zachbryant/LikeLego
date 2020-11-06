5702e5eae079d520aa1c6a03f7d4438300c50982
Zach Bryant <zdb282@gmail.com>
2020-11-06 01:54:52 -0800

    jump-off loader to illustrate an approach

--- server/src/loaders/app.ts
+++ server/src/loaders/app.ts
@@ -1,61 +1,19 @@
 import { modeTag } from '@config/';
 import { AbstractLoader } from '@interfaces/loader';
 import { logStrings } from '@strings/';
-import { appLoadWelcome } from '@strings/logging';
-
-type VoidAny = void | any;
+import { appLoadWelcome, initAllLoaders } from '@strings/logging';
 
 /**
- * @deprecated
- * This is the main loader, which loads all other loaders.
- * Loaders marked synchronous will be loaded as such, and others
- * will be loaded concurrently.
+ * This loader is used as a jumping-off point for the rest of the app.
  */
-export class AppLoader extends AbstractLoader<VoidAny> {
-    private asyncLoaders;
-    private syncLoaders;
-
-    constructor(loaders: AbstractLoader<VoidAny>[]) {
+export class AppLoader extends AbstractLoader {
+    constructor() {
         super();
-        loaders = loaders.filter(Boolean);
-        //this.syncLoaders = loaders.filter((l) => !l.loadAsync);
-        //this.asyncLoaders = loaders.filter((l) => l.loadAsync);
     }
 
     async load() {
-        this.welcome();
-        this.log.info(`Running in ${modeTag} mode`);
-        this.log.info(logStrings.initAllLoaders);
-
-        await this.loadSynchronous();
-
-        const loaderPromises = this.asyncLoaders.map((loader) =>
-            loader.start(),
-        );
-        return Promise.all(loaderPromises)
-            .then(() => {
-                this.log.info(logStrings.doneAllLoaders);
-            })
-            .catch(this.failed);
-    }
-
-    async loadSynchronous() {
-        await this.syncLoaders.reduce(
-            (
-                prevPromise: Promise<VoidAny>,
-                curLoader: AbstractLoader<VoidAny>,
-            ) => {
-                return prevPromise.then(() => curLoader.start());
-            },
-            Promise.resolve(),
-        );
-    }
-
-    protected inject() {
-        super.inject();
-    }
-
-    private welcome() {
         this.log.info(appLoadWelcome);
+        this.log.info(`Running in ${modeTag} mode`);
+        this.log.info(initAllLoaders);
     }
 }
